3b2bc1273ab27a88eeab7023ae1441ef99c2cc2b
Igor Ovchinnikov <faceplaces@inbox.ru>
2023-07-26 16:15:40 +0300

    first commit

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,4 +1,4 @@
-import App from "../app";
+import GamePlay from "../gameplay.js";
 
 test("new App(index) should generate board index**2", () => {
   expect(document.getElementsByClassName("hole").length).toBe(0);
